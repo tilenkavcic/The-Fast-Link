f1bb76e0e8540c24b2c13c72fc7e392de0b5f759
Tilen Kavčič <tilen.kavcic@outlook.com>
2021-03-21 23:55:53 +0100

    Refactor admin links

---
+++ components/AdminLinks.js
@@ -0,0 +1,89 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useAuthUser, withAuthUser, withAuthUserTokenSSR, AuthAction } from "next-firebase-auth";
+import Link from "next/link";
+import getAbsoluteURL from "../utils/getAbsoluteURL";
+import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
+
+const AdminLinks = ({ pageData, setPageData }) => {
+	const AuthUser = useAuthUser();
+
+	const uploadData = useCallback(
+		async (data) => {
+			const token = await AuthUser.getIdToken();
+			const endpoint = getAbsoluteURL("/api/uploadPageData");
+			const response = await fetch(endpoint, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: token,
+					uid: AuthUser.id,
+				},
+				body: JSON.stringify(data),
+			});
+			const respData = await response.json();
+			if (!response.ok) {
+				console.error(`Data fetching failed with status ${response.status}: ${JSON.stringify(respData)}`);
+				return null;
+			}
+			return respData;
+		},
+		[AuthUser]
+	);
+
+	const isRequired = (message) => (value) => (!!value ? undefined : message);
+
+	return (
+		<Formik
+			initialValues={pageData}
+			onSubmit={async (values) => {
+				console.log("submiting:", values);
+				const ret = await uploadData(values);
+				setPageData(values);
+				console.log("uploaded", pageData);
+			}}
+		>
+			{({ values }) => (
+				<Form>
+					<FieldArray name="links">
+						{({ insert, remove, push, move }) => (
+							<div>
+								{values.links.length > 0 &&
+									values.links.map((friend, index) => (
+										<div className="row" key={index}>
+											<div className="col">
+												<label htmlFor={`links.${index}.title`}>Name</label>
+												<Field name={`links.${index}.title`} placeholder="Link title" type="text" />
+												<ErrorMessage name={`links.${index}.title`} component="div" className="field-error" />
+											</div>
+											<div className="col">
+												<label htmlFor={`links.${index}.url`}>Link URL</label>
+												<Field name={`links.${index}.url`} validate={isRequired("URL is required")} placeholder="https://myPodcast.com" type="url" />
+												<ErrorMessage name={`links.${index}.title`} component="div" className="field-error" />
+											</div>
+											<div className="col">
+												<label htmlFor={`links.${index}.pictureUrl`}>pictureUrl</label>
+												<Field name={`links.${index}.pictureUrl`} placeholder="www.picture.com" type="url" />
+												<img src={friend.pictureUrl} alt={`links.${index}.title`} />
+												<ErrorMessage name={`links.${index}.title`} component="div" className="field-error" />
+											</div>
+											<div className="col">
+												<button type="button" className="secondary" onClick={() => remove(index)}>
+													X
+												</button>
+											</div>
+										</div>
+									))}
+								<button type="button" className="secondary" onClick={() => push({ title: "", url: "", pictureUrl: "" })}>
+									Add Link
+								</button>
+							</div>
+						)}
+					</FieldArray>
+					<button type="submit">Save</button>
+				</Form>
+			)}
+		</Formik>
+	);
+};
+
+export default AdminLinks;
--- pages/admin.js
+++ pages/admin.js
@@ -1,15 +1,12 @@
-import React, { useCallback, useEffect, useState, createContext } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useAuthUser, withAuthUser, withAuthUserTokenSSR, AuthAction } from "next-firebase-auth";
 import Link from "next/link";
 import Head from "next/head";
 import Header from "../components/Header";
 import FullPageLoader from "../components/FullPageLoader";
 import getAbsoluteURL from "../utils/getAbsoluteURL";
-// import { Form, TextField, SelectField, SubmitButton } from "../components/FormElements";
 import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
-import * as Yup from "yup";
-
-// export const PageContext = createContext();
+import AdminLinks from "../components/AdminLinks";
 
 const Page = () => {
 	const AuthUser = useAuthUser();
@@ -33,29 +30,6 @@ const Page = () => {
 		return data;
 	}, [AuthUser]);
 
-	const uploadData = useCallback(
-		async (data) => {
-			const token = await AuthUser.getIdToken();
-			const endpoint = getAbsoluteURL("/api/uploadPageData");
-			const response = await fetch(endpoint, {
-				method: "POST",
-				headers: {
-					headers: { "Content-Type": "application/json" },
-					Authorization: token,
-					uid: AuthUser.id,
-				},
-				body: JSON.stringify(data),
-			});
-			const respData = await response.json();
-			if (!response.ok) {
-				console.error(`Data fetching failed with status ${response.status}: ${JSON.stringify(respData)}`);
-				return null;
-			}
-			return respData;
-		},
-		[AuthUser]
-	);
-	const isRequired = (message) => (value) => (!!value ? undefined : message);
 	useEffect(() => {
 		const fetchPageData = async () => {
 			const data = await fetchData();
@@ -77,59 +51,7 @@ const Page = () => {
 						<img src={pageData.pictureUrl} alt={pageData.title} />
 					</div>
 
-					{pageData.title ? (
-						<Formik
-							initialValues={pageData}
-							onSubmit={async (values) => {
-								console.log("submiting:", values);
-								const ret = await uploadData(values);
-								console.log("uploaded", ret);
-							}}
-						>
-							{({ values }) => (
-								<Form>
-									<FieldArray name="links">
-										{({ insert, remove, push }) => (
-											<div>
-												{values.links.length > 0 &&
-													values.links.map((friend, index) => (
-														<div className="row" key={index}>
-															<div className="col">
-																<label htmlFor={`links.${index}.title`}>Name</label>
-																<Field name={`links.${index}.title`} placeholder="Link title" type="text" />
-																<ErrorMessage name={`links.${index}.title`} component="div" className="field-error" />
-															</div>
-															<div className="col">
-																<label htmlFor={`links.${index}.url`}>Link URL</label>
-																<Field name={`links.${index}.url`} validate={isRequired("URL is required")} placeholder="https://myPodcast.com" type="url" />
-																<ErrorMessage name={`links.${index}.title`} component="div" className="field-error" />
-															</div>
-															<div className="col">
-																<label htmlFor={`links.${index}.pictureUrl`}>pictureUrl</label>
-																<Field name={`links.${index}.pictureUrl`} placeholder="www.picture.com" type="url" />
-																<img src={friend.pictureUrl} alt={`links.${index}.title`} />
-																<ErrorMessage name={`links.${index}.title`} component="div" className="field-error" />
-															</div>
-															<div className="col">
-																<button type="button" className="secondary" onClick={() => remove(index)}>
-																	X
-																</button>
-															</div>
-														</div>
-													))}
-												<button type="button" className="secondary" onClick={() => push({ title: "", url: "", pictureUrl: "" })}>
-													Add Link
-												</button>
-											</div>
-										)}
-									</FieldArray>
-									<button type="submit">Save</button>
-								</Form>
-							)}
-						</Formik>
-					) : (
-						""
-					)}
+					{pageData.title ? <AdminLinks pageData={pageData} setPageData={setPageData} /> : ""}
 				</div>
 			</div>
 		</>
