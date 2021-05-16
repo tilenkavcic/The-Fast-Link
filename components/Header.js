dae9b60dc16097b46404528aa6468a6c96772fe1
Tilen Kavčič <tilen.kavcic@outlook.com>
2021-05-16 03:57:28 +0200

    Add react styling

--- components/Header.js
+++ components/Header.js
@@ -1,47 +1,44 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import { Button } from "react-bootstrap";
 
 const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    padding: 16,
-  },
-  button: {
-    marginLeft: 16,
-    cursor: 'pointer',
-  },
-}
+	container: {
+		display: "flex",
+		justifyContent: "flex-end",
+		alignItems: "center",
+		padding: 16,
+	},
+};
 
 const Header = ({ email, signOut }) => (
-  <div style={styles.container}>
-    {email ? (
-      <>
-        <p>Signed in as {email}</p>
-        <button
-          type="button"
-          onClick={() => {
-            signOut()
-          }}
-          style={styles.button}
-        >
-          Sign out
-        </button>
-      </>
-    ) : (
-      <>
-        <p>You are not signed in.</p>
-        <Link href="/auth">
-          <a>
-            <button type="button" style={styles.button}>
-              Sign in
-            </button>
-          </a>
-        </Link>
-      </>
-    )}
-  </div>
-)
+	<div style={styles.container}>
+		{email ? (
+			<>
+				<p>Signed in as {email}</p>
+				<Button
+          variant="primary"
+					type="button"
+					onClick={() => {
+						signOut();
+					}}
+				>
+					Sign out
+				</Button>
+			</>
+		) : (
+			<>
+				<p>You are not signed in.</p>
+				<Link href="/auth">
+					<a>
+						<Button variant="primary" type="button">
+							Sign in
+						</Button>
+					</a>
+				</Link>
+			</>
+		)}
+	</div>
+);
 
-export default Header
+export default Header;
