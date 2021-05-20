82756a650602a04f78ab3fb79fabaa65efeab66f
Tilen Kavčič <tilen.kavcic@outlook.com>
2021-05-20 20:46:24 +0200

    Fix calling firebase with user instead of admin

--- pages/api/getUserData.js
+++ pages/api/getUserData.js
@@ -1,6 +1,6 @@
 import { useAuthUser, withAuthUser, withAuthUserTokenSSR, verifyIdToken } from "next-firebase-auth";
 import initAuth from "../../utils/initAuth";
-import firebase from "../../firebase/clientApp";
+import firebase from "../../firebase/adminApp";
 import "firebase/firestore";
 
 initAuth();
@@ -10,18 +10,26 @@ const handler = async (req, res) => {
 		res.status(400).json({ error: "Missing Authorization header value" });
 	}
 	const token = req.headers.authorization;
+	const uid = req.headers.uid;
+
 	if (token != "unauthenticated") {
 		// verify login
 		try {
-			await verifyIdToken(token);
+			const authUser = await verifyIdToken(token);
+			console.log(authUser);
+			if (authUser.id != uid) {
+				throw "Page outside of user scope";
+			}
 		} catch (e) {
 			console.error(e);
 			res.status(403).json({ error: "Not authorized" });
 		}
 		// Get data from firestore
 		try {
-			const uid = req.headers.uid;
-			const userData = await firebase.firestore().collection("users").doc(uid).get();
+			const ref = firebase.firestore().collection("users").doc(uid);
+			const userData = await ref.get();
+			console.log("userData", userData);
+
 			res.status(200).json(userData.data());
 		} catch (e) {
 			console.error("Error getting user data");
