204970c36ee262a185a69827c0b1e0c742b0ac5a
Tilen Kavčič <tilen.kavcic@outlook.com>
2021-06-03 22:49:05 +0200

    Add footer bug report, feedback typeform

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,23 @@
 import styles from "./footer.module.scss";
 import Link from "next/link";
-import { Row } from "react-bootstrap";
+import { Row, Button, Col } from "react-bootstrap";
+import { useEffect } from "react";
 
 export default function Footer() {
 	return (
-		<Row className={styles.wrapper}>
-			<Link href="/">The Fast Link</Link>
-		</Row>
+		<>
+			<Row className={styles.wrapper}>
+				<Link href="/">The Fast Link</Link>
+			</Row>
+			<Row className={styles.feedback}>
+				<a href="https://form.typeform.com/to/zovRfPGp?typeform-medium=embed-snippet" data-hide-footer="true" target="_blank">
+					Report a bug
+				</a>
+				&emsp;
+				<a href="https://form.typeform.com/to/zovRfPGp?typeform-medium=embed-snippet" data-hide-footer="true" target="_blank">
+					Feedback
+				</a>
+			</Row>
+		</>
 	);
 }
